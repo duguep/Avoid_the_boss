9c6ff0220629ddff6a39b4709112739de5704730
rodolphe <rodolphe.dugueperoux@epitech.eu>
2019-01-15 17:31:08 +0100

    Oculus intégration

---
+++ AvoidTheBoss/Assets/ObjectAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAction : MonoBehaviour
+{
+    private OVRGrabbable bla;
+    private bool grabbed = false;
+
+    private bool action = false;
+    
+    private void Start()
+    {
+        bla = GetComponent<OVRGrabbable>();
+    }
+
+    private void Update()
+    {
+        if (!bla.isGrabbed && grabbed == true)
+            action = true;
+        else
+        {
+            grabbed = bla.isGrabbed;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (action)
+        {
+            
+            print("boom" + other.gameObject.name);
+            Destroy(this.gameObject);            
+        }
+        
+
+    }
+}
+
---
+++ AvoidTheBoss/Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+	public GameObject powerUpPrefabs;
+
+	private GameObject instance = null;
+
+	void Update ()
+	{
+		if (instance == null)
+			instance = Instantiate(powerUpPrefabs, transform);
+	}
+}
