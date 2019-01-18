8f3b8b4384f659425da3ddf11c00a2914c9350a8
Pierrick FABRY <pierrick.fabry35@gmail.com>
2019-01-18 16:42:13 +0100

    Before Game
    Intégration models IA
    Design et link des menus avant game
    correction animations alertes patron
    Resize de la room

---
+++ AvoidTheBoss/Assets/Scripts/PanelsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsManager : MonoBehaviour
+{
+
+	public void ClosePanel(GameObject actualPanel)
+	{
+		actualPanel.SetActive(false);
+	}
+
+	public void OpenPanel(GameObject wantedPanel)
+	{
+		wantedPanel.SetActive(true);
+	}
+}
