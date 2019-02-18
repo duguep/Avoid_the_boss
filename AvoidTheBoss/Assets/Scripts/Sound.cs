2e13a9aab08aa41a82eb71f01232a83c6abfbc5e
Pierrick FABRY <pierrick.fabry35@gmail.com>
2019-02-18 09:10:50 +0100

    Sounds

---
+++ AvoidTheBoss/Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+
+    public string Name;
+    
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float Volume;
+    [Range(.1f, 3f)]
+    public float Pitch;
+
+    public bool loop;
+    
+    [HideInInspector]
+    public AudioSource source;
+}
