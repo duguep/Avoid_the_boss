04c11e494f7f0be9ac7c9f71bb96b98a7df138cb
Pierrick FABRY <pierrick.fabry35@gmail.com>
2019-03-19 16:31:03 +0100

    grab all & display prices

---
+++ AvoidTheBoss/Assets/Scripts/DispPrice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DispPrice : MonoBehaviour
+{
+    [SerializeField] private GameObject _panelToDisp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("name enter = " + other.name);        
+        if (other.name == "LeftGrabber" || other.name == "RightGrabber")
+            _panelToDisp.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            _panelToDisp.SetActive(false);
+    }
+}
