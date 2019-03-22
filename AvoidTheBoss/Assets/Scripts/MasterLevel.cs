72968b18173502f2a31cc9d83d80433f697a46a1
Pierrick FABRY <pierrick.fabry35@gmail.com>
2019-03-22 15:37:08 +0100

    Modifiy menu demarrer et co

---
+++ AvoidTheBoss/Assets/Scripts/MasterLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+
+public class MasterLevel : MonoBehaviour
+{
+
+	[SerializeField] private AudioMixer _masterMix;
+
+	public void SetMasterVolume(float vol)
+	{
+		_masterMix.SetFloat("MasterVolume", vol);
+	}
+}
---
+++ AvoidTheBoss/Assets/Scripts/setOVRight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setOVRight : MonoBehaviour
+{
+	public CurvedUIInputModule CurvedUiInputModule;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!CurvedUiInputModule.OculusCameraRig)
+			CurvedUiInputModule.OculusCameraRig = GameObject.Find("OVRCameraRig").GetComponent<OVRCameraRig>();	
+	}
+}
